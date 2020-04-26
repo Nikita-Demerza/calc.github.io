cefb65561c41a09af2aa6a9711d701536c7f6e8a
root <root@localhost>
2020-04-26 23:00:30 +0300

    publishing awesome pages 1587931230277

--- js/main.js
+++ js/main.js
@@ -5,8 +5,36 @@ function a(i){
 function del(){
     $("#result").empty();
 }
+function back(){
+    document.getElementById("result").innerHTML =document.getElementById("result").innerHTML.substring(0,document.getElementById("result").innerHTML.length-1);
+}
 function eq(){
     var h = $("#result").text();
     var g = eval(h);
     $("#result").text(g);
 }
+function sin(num){
+    return Math.sin(num)
+}
+function cos(num){
+    return Math.cos(num)
+}
+function tan(num){
+    return Math.tan(num)
+}
+function cot(num){
+    return 1/Math.tan(num)
+}
+function asin(num){
+    return Math.asin(num)
+}
+function acos(num){
+    return Math.acos(num)
+}
+function atan(num){
+    return Math.atan(num)
+}
+function acot(num){
+    return Math.PI / 2 - Math.atan(num)
+}
+
